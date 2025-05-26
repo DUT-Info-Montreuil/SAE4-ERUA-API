c8c1b70d3c968e76c751a9dee5c05ce769d7990c
Fabrice95500 <fabricecannan@outlook.com>
2025-05-27 00:35:56 +0200

    feat : add endpoint artworks by id and add metadonnées

--- controllers/artwork_controller.py
+++ controllers/artwork_controller.py
@@ -10,11 +10,19 @@ def get_artwork() -> tuple[Response, int]:
     artwork = artwork_service.get_artwork()
     return send_response(data=artwork)
 
+@artwork_controller.route('/<int:artwork_id>', methods=['GET'])
+def get_artwork_by_id(artwork_id: int) -> tuple[Response, int]:
+    artwork = artwork_service.get_artwork_by_id(artwork_id)
+    if artwork:
+        return send_response(data=artwork)
+    else:
+        return send_error(status=404, message="Artwork not found")
+
 
 @artwork_controller.route('', methods=['POST'])
 def post_artwork() -> tuple[Response, int]:
 
-    # Required field : Art_Title, Art_Year, Art_Description
+    # Required field : Art_Title, Art_Year, Art_Description, Art_ImageURL, Art_Medium, Art_Dimensions
     # Optionnal field : NULL
 
     data = request.get_json()
@@ -28,24 +36,28 @@ def post_artwork() -> tuple[Response, int]:
     if not data or 'Art_Description' not in data:
         return send_error(status=400, message="Art_Description is required")
     
-    if not data or 'Ar_FirstName' not in data:
-        return send_error(status=400, message="Ar_FirstName is required")
+    if not data or 'Art_ImageURL' not in data:
+        return send_error(status=400, message="Art_ImageURL is required")
     
-    if not data or 'Ar_LastName' not in data:
-        return send_error(status=400, message="Ar_LastName is required")
+    if not data or 'Art_Medium' not in data:
+        return send_error(status=400, message="Art_Medium is required")
     
+    if not data or 'Art_Dimensions' not in data:
+        return send_error(status=400, message="Art_Dimensions is required")
 
     Art_Title: str = data['Art_Title']
     Art_Year: str = data['Art_Year']
     Art_Description: str = data['Art_Description']
-    Ar_FirstName: str = data['Ar_FirstName']
-    Ar_LastName: str = data['Ar_LastName']
+    Art_ImageURL: str = data['Art_ImageURL']
+    Art_Medium: str = data['Art_Medium']
+    Art_Dimensions: str = data['Art_Dimensions']
+
 
     if not check_date(Art_Year):
         return send_error(status=400, message="Art_Year must be a valid date")
 
     try:
-        new_artwork = artwork_service.post_artwork(Art_Title, Art_Year, Art_Description, Ar_FirstName, Ar_LastName)
+        new_artwork = artwork_service.post_artwork(Art_Title, Art_Year, Art_Description, Art_ImageURL, Art_Medium, Art_Dimensions)
         return send_response(status=201, messages="Artwork created", data=new_artwork)
     except Exception as e:
         return send_error(status=500, message="An unexpected error occurred. Please try again later.")
--- services/artwork_service.py
+++ services/artwork_service.py
@@ -7,18 +7,32 @@ def get_artwork():
 
     return artwork
 
-def post_artwork(Art_Title: str, Art_Year: int, Art_Description: str, Ar_FirstName: str, Ar_LastName: str):
+def get_artwork_by_id(Ar_ArtworkID: int):
     query = """
-    MATCH (a:Artist {Ar_FirstName: $Ar_FirstName, Ar_LastName: $Ar_LastName})
-    CREATE (a)-[:CREATED]->(aw:Artwork {Art_Title: $Art_Title, Art_Year: $Art_Year, Art_Description: $Art_Description})
+    MATCH (a:Artwork {Ar_ArtworkID: $Ar_ArtworkID})
+    RETURN a
+    """
+    results = execute_query(query=query, parameters={'Ar_ArtworkID': Ar_ArtworkID})
+    if results:
+        return results[0]['a']
+    return None
+
+def post_artwork(Art_Title: str, Art_Year: int, Art_Description: str, Art_ImageURL: str, Art_Medium: str, Art_Dimensions: str):
+    query = """
+    MERGE (c:Counter {name: 'Art_ArtworkID'})
+    ON CREATE SET c.count = 0
+    SET c.count = c.count + 1
+    WITH c.count AS new_id
+    CREATE (a)-[:CREATED]->(aw:Artwork {Art_ArtworkID: new_id, Art_Title: $Art_Title, Art_Year: $Art_Year, Art_Description: $Art_Description, Art_ImageURL: $Art_ImageURL, Art_Medium: $Art_Medium, Art_Dimensions: $Art_Dimensions})
     RETURN aw AS Artwork
     """
     params = {
         'Art_Title': Art_Title,
         'Art_Year': Art_Year,
         'Art_Description': Art_Description,
-        'Ar_FirstName': Ar_FirstName,
-        'Ar_LastName': Ar_LastName
+        'Art_ImageURL': Art_ImageURL,
+        'Art_Medium': Art_Medium,
+        'Art_Dimensions': Art_Dimensions
     }
 
     results = execute_query(query=query, parameters=params)
