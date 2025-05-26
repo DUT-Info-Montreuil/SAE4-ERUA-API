d6391b61e9a6286bd75d1de1899f58a56c2e907b
Fabrice95500 <fabricecannan@outlook.com>
2025-05-27 00:10:56 +0200

    feat : add endpoint artists by id and add metadonnées to artists

--- controllers/artist_controller.py
+++ controllers/artist_controller.py
@@ -10,12 +10,21 @@ def get_artists() -> tuple[Response, int]:
     artists = artist_service.get_artists()
     return send_response(data=artists)
 
+@artist_controller.route('/<int:artist_id>', methods=['GET'])
+def get_artist_by_id(artist_id: int) -> tuple[Response, int]:
+    artist = artist_service.get_artist_by_id(artist_id)
+    if artist:
+        return send_response(data=artist)
+    else:
+        return send_error(status=404, message="Artist not found")
+
+
 
 @artist_controller.route('', methods=['POST'])
 def post_artist() -> tuple[Response, int]:
 
-    # Required field : Ar_FirstName, Ar_LastName, Ar_BirthDay
-    # Optionnal field : NULL
+    # Required field : Ar_FirstName, Ar_LastName, Ar_BirthDay, Ar_Nationality, Ar_Biography, Ar_ImageURL
+    # Optionnal field : Ar_DeathDay
 
     data = request.get_json()
 
@@ -27,17 +36,38 @@ def post_artist() -> tuple[Response, int]:
     
     if not data or 'Ar_BirthDay' not in data:
         return send_error(status=400, message="Ar_BirthDay is required")
+    
+    if not data or 'Ar_Nationality' not in data:
+        return send_error(status=400, message="Ar_Nationality is required")
+    
+    if not data or 'Ar_Biography' not in data:
+        return send_error(status=400, message="Ar_Biography is required")
+    
+    if not data or 'Ar_ImageURL' not in data:
+        return send_error(status=400, message="Ar_ImageURL is required")
 
     
     Ar_FirstName: str = data['Ar_FirstName']
     Ar_LastName: str = data['Ar_LastName']
     Ar_BirthDay: str = data['Ar_BirthDay']
+    Ar_Nationality: str = data['Ar_Nationality']
+    Ar_Biography: str = data['Ar_Biography']
+    Ar_ImageURL: str = data['Ar_ImageURL']
+
+    Ar_DeathDay = data.get('Ar_DeathDay', "") 
 
     if not check_date(Ar_BirthDay):
         return send_error(status=400, message="Ar_BirthDay must be a valid date")
+    
+    if Ar_DeathDay and not check_date(Ar_DeathDay):
+        return send_error(status=400, message="Ar_BirthDay must be a valid date")
 
     try:
-        new_artist = artist_service.post_artist(Ar_FirstName, Ar_LastName, Ar_BirthDay)
-        return send_response(status=201, messages="Artist created", data=new_artist)
+        new_artist = artist_service.post_artist(Ar_FirstName, Ar_LastName, Ar_BirthDay, Ar_Nationality, Ar_Biography, Ar_ImageURL, Ar_DeathDay)
+
+        if new_artist:
+            return send_response(status=201, messages="Artist created", data=new_artist)
+        else:
+            return send_error(status=500, message="Failed to create artist. Please try again later.")
     except Exception as e:
         return send_error(status=500, message="An unexpected error occurred. Please try again later.")
--- services/artist_service.py
+++ services/artist_service.py
@@ -9,19 +9,49 @@ def get_artists():
     return artists
 
 
-def post_artist(Ar_FirstName: str, Ar_LastName: str, Ar_BirthDay: str):
+def post_artist(Ar_FirstName: str, Ar_LastName: str, Ar_BirthDay: str, Ar_Nationality: str, Ar_Biography: str, Ar_ImageURL: str, Ar_DeathDay: str = ""):
     query = """
-    CREATE (a:Artist {Ar_FirstName: $Ar_FirstName, Ar_LastName: $Ar_LastName, Ar_BirthDay: $Ar_BirthDay})
-    RETURN a AS Artist
+    MERGE (c:Counter {name: 'Ar_ArtistID'})
+    ON CREATE SET c.count = 0
+    SET c.count = c.count + 1
+    WITH c.count AS new_id
+    CREATE (a:Artist {
+        Ar_ArtistID: new_id,
+        Ar_FirstName: $Ar_FirstName,
+        Ar_LastName: $Ar_LastName,
+        Ar_BirthDay: $Ar_BirthDay,
+        Ar_Nationality: $Ar_Nationality,
+        Ar_Biography: $Ar_Biography,
+        Ar_ImageURL: $Ar_ImageURL,
+        Ar_DeathDay: $Ar_DeathDay
+    })
+    RETURN a
     """
-    
+
     params = {
         'Ar_FirstName': Ar_FirstName,
         'Ar_LastName': Ar_LastName,
-        'Ar_BirthDay': Ar_BirthDay
+        'Ar_BirthDay': Ar_BirthDay,
+        'Ar_Nationality': Ar_Nationality,
+        'Ar_Biography': Ar_Biography,
+        'Ar_ImageURL': Ar_ImageURL,
+        'Ar_DeathDay': Ar_DeathDay
     }
 
     results = execute_query(query=query, parameters=params)
     if results:
-        return results[0]['Artist']
+        return results[0]['a']
     return None
+
+
+def get_artist_by_id(Ar_ArtistID: int):
+    query = """
+    MATCH (a:Artist {Ar_ArtistID: $Ar_ArtistID})
+    RETURN a
+    """
+    results = execute_query(query=query, parameters={'Ar_ArtistID': Ar_ArtistID})
+    if results:
+        return results[0]['a']
+    return None
+
+
