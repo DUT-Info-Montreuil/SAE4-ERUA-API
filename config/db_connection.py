358b3865b6b2d77106571c2db5e54f814f1c6a73
FaabriceC <fabricecannan@outlook.com>
2025-05-27 11:36:22 +0200

    feat : add .env

--- config/db_connection.py
+++ config/db_connection.py
@@ -1,21 +1,21 @@
-import configparser
+import os
 from neo4j import GraphDatabase
+from dotenv import load_dotenv
 
-config = configparser.ConfigParser()
-config.read('config/config.ini')
+load_dotenv()
 
-URI = config['URI']['iut']
-AUTH = (config['credentials']['user'], config['credentials']['pwd'])
+URI = os.getenv("NEO4J_URL")
+AUTH = (os.getenv("NEO4J_USER"), os.getenv("NEO4J_PASSWORD"))
 
-with GraphDatabase.driver(URI, auth = AUTH) as driver:
+with GraphDatabase.driver(URI, auth=AUTH) as driver:
     driver.verify_connectivity()
 
 
 def execute_query(query: str, parameters: dict = None):
     try:
         with driver.session() as session:
-            result: str = session.run(query, parameters or {})
-            print(result)
+            result = session.run(query, parameters or {})
             return [record.data() for record in result]
     except Exception as e:
+        print(f"Erreur lors de l'exécution de la requête : {e}")
         return None
